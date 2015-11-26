20d4eab90ec60e77873814be291f6194e92b5996
MohammadTazehkar <mohammad.tazehkar@gmail.com>
2015-11-26 18:29:56 +0330

    first commit

---
+++ app/src/main/java/com/example/mohammad/mystartup13940905/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.mohammad.mystartup13940905;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private ActionBar actionBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            actionBar = getSupportActionBar();
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+            actionBar.setIcon(R.drawable.ic_done_white_24dp);
+            actionBar.setTitle("");
+
+
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+}
